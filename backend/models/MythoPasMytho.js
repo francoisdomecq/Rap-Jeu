af5e9cf3dfe3299eeea7802eceb4ae030189bf55
francoisdomecq <fradomecq@gmail.com>
2022-03-03 18:02:27 +0100

    models files

--- backend/models/MythoPasMytho.js
+++ backend/models/MythoPasMytho.js
@@ -4,6 +4,7 @@ const mythoPasMythoSchema = mongoose.Schema({
     question : {type:String, required:true},
     reponse : {type:String,required:true},
     illustration : {type:String,default:null},
+    type:{type:String},
 })
 
 module.exports=mongoose.model('MythoPasMytho',mythoPasMythoSchema)
--- backend/models/RapGenieOuGenant.js
+++ backend/models/RapGenieOuGenant.js
@@ -4,6 +4,7 @@ const rapGenieOuGenantSchema = mongoose.Schema({
     question : {type:String, required:true},
     reponse : {type:String,required:true},
     illustration : {type:String,default:null},
+    type:{type:String},
 })
 
 module.exports=mongoose.model('RapGenieOuGenant',rapGenieOuGenantSchema)
