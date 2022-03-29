71259ea12b751a239d7691d0e546730a8637a6a2
francoisdomecq <fradomecq@gmail.com>
2022-03-29 16:17:48 +0200

    CrossFeaturing

--- frontend/src/pages/Games/CrossFeaturing/index.jsx
+++ frontend/src/pages/Games/CrossFeaturing/index.jsx
@@ -7,25 +7,64 @@ import ScoreTeam2 from '../../../components/Score/index.scoreteam2'
 import CrossFeaturingArray from '../../../components/CrossFeaturing'
 import HasGameStarted from '../../../utils/functions/hasGameStarted'
 
-import { SecondContainer } from './styles'
+import {
+  RappeurContainer,
+  SecondContainer,
+  InputContainer,
+  SearchContainer,
+  Text,
+  RapperInput,
+  SearchInput,
+  Header,
+  RappersContainer,
+  TableBottom,
+} from './styles'
 import { ContainerRow, ContainerColumn } from '../../../utils/styles/balises'
 import '../../../utils/animations/Bouncing/crossFeaturingBouncingLetters.css'
 import '../../../utils/animations/Bouncing/animationBouncing.css'
 
+import MusicBrainzLogo from '../../../assets/rollandgamos/MusicBrainz_Logo.png'
+import YoutubeLogo from '../../../assets/rollandgamos/icons8-youtube.svg'
+import RapGeniusLogo from '../../../assets/rollandgamos/RapGenius_Logo.png'
+
 function CrossFeaturing() {
   const [crossFeaturing, setCrossFeaturing] = useState()
   const [answerNumber, updateAnswerNumber] = useState(0)
+  const [rappersTeam1, setRappersTeam1] = useState([])
+  const [rappersTeam2, setRappersTeam2] = useState([])
+  const [rappeur1Search, setRappeur1Search] = useState('')
+  const [rappeur2Search, setRappeur2Search] = useState('')
   const { games, updateGamesPlayed } = useContext(GameContext)
 
   function selectCrossFeaturing(crossFeaturing) {
     setCrossFeaturing(crossFeaturing)
   }
+  function addRappers(e, team) {
+    if (e.key === 'Enter') {
+      if (team === 'team1') {
+        rappersTeam1.push(e.target.value)
+        const newRapper = [...rappersTeam1]
+        setRappersTeam1(newRapper)
+        e.target.value = ''
+      } else {
+        rappersTeam2.push(e.target.value)
+        const newRapper = [...rappersTeam2]
+        setRappersTeam2(newRapper)
+        e.target.value = ''
+      }
+    }
+  }
   const updateAnswer = () => {
     updateGamesPlayed('Le CrossFeaturing', answerNumber, updateAnswerNumber)
+    setCrossFeaturing()
+    setRappersTeam1([])
+    setRappersTeam2([])
   }
   // HasGameStarted()
   return (
     <ContainerRow>
+      {/* Plutôt que tableau faire truc en ligne*/}
+      {console.log(rappersTeam1, rappersTeam2)}
       <div className="bouncing-text">
         <div className="l-cf">l</div>
         <div className="e-cf">e</div>
@@ -49,9 +88,106 @@ function CrossFeaturing() {
         <ContainerRow>
           <ScoreTeam1 value={5} />
           <ScoreTeam2 value={5} />
-          <SecondContainer>
-            <CrossFeaturingArray selectCrossFeaturing={selectCrossFeaturing} />
-          </SecondContainer>
+
+          {!crossFeaturing ? (
+            <SecondContainer>
+              <CrossFeaturingArray
+                selectCrossFeaturing={selectCrossFeaturing}
+              />
+            </SecondContainer>
+          ) : (
+            <ContainerRow>
+              <ContainerRow style={{ marginTop: '2%' }}>
+                <ContainerColumn style={{ width: '43%' }}>
+                  <Text>Equipe 1</Text>
+                  <Header>Rappeurs cités</Header>
+                  <RappersContainer>
+                    {rappersTeam1.length > 0
+                      ? rappersTeam1.map((rapper) => <p>{rapper}</p>)
+                      : null}
+                  </RappersContainer>
+                  <TableBottom />
+                  <Text>Nouveau rappeur</Text>
+                  <RapperInput
+                    type="search"
+                    onKeyPress={(e) => addRappers(e, 'team1')}
+                  />
+                </ContainerColumn>
+                <ContainerRow style={{ width: '14%' }}>
+                  <Text>
+                    {crossFeaturing.rappeur1}-{crossFeaturing.rappeur2}
+                  </Text>
+                </ContainerRow>
+                <ContainerColumn style={{ width: '43%' }}>
+                  <Text>Equipe 2</Text>
+                  <Header>Rappeurs cités</Header>
+                  <RappersContainer>
+                    {rappersTeam2.length > 0
+                      ? rappersTeam2.map((rapper) => <p>{rapper}</p>)
+                      : null}
+                  </RappersContainer>
+                  <TableBottom />
+
+                  <Text>Nouveau rappeur</Text>
+                  <RapperInput
+                    type="search"
+                    onKeyPress={(e) => addRappers(e, 'team2')}
+                  />
+                </ContainerColumn>
+              </ContainerRow>
+              <SecondContainer>
+                <Text>Un doute sur un featuring ? </Text>
+                <InputContainer>
+                  <SearchInput
+                    type="search"
+                    placeholder={rappeur1Search}
+                    value={rappeur1Search}
+                    onChange={(e) => setRappeur1Search(e.target.value)}
+                  />
+                  <SearchInput
+                    type="search"
+                    placeholder="rappeur 2"
+                    onChange={(e) => setRappeur2Search(e.target.value)}
+                  />
+                </InputContainer>
+                <SearchContainer>
+                  <a
+                    rel="noreferrer"
+                    href={`https://www.youtube.com/results?search_query=${rappeur1Search}+${rappeur2Search}`}
+                    target="_blank"
+                  >
+                    <img
+                      style={{ width: 96, height: 96 }}
+                      src={YoutubeLogo}
+                      alt="Chercher sur youtube"
+                    />
+                  </a>
+                  <a
+                    rel="noreferrer"
+                    href={`https://musicbrainz.org/search?query=${rappeur1Search}%20feat.%20${rappeur2Search}&type=recording&limit=5&method=advanced`}
+                    target="_blank"
+                  >
+                    <img
+                      src={MusicBrainzLogo}
+                      style={{ width: 96, heigth: 96 }}
+                      alt="Chercher sur MusicBrainz"
+                    />
+                  </a>
+                  <a
+                    rel="noreferrer"
+                    href={`https://genius.com/search?q=${rappeur1Search}%20${rappeur2Search}`}
+                    target="_blank"
+                  >
+                    <img
+                      src={RapGeniusLogo}
+                      style={{ width: 96, heigth: 96 }}
+                      alt="Chercher sur RapGenius"
+                    />
+                  </a>
+                </SearchContainer>
+              </SecondContainer>
+            </ContainerRow>
+          )}
         </ContainerRow>
         <ContainerColumn style={{ width: 50, height: 50 }}>
           {answerNumber < 2 ? (
--- frontend/src/pages/Games/CrossFeaturing/styles.jsx
+++ frontend/src/pages/Games/CrossFeaturing/styles.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import colors from '../../../utils/styles/colors'
+import RapperInputBG from '../../../assets/rollandgamos/rappeurInput.svg'
+import RapperContainerBG from '../../../assets/rollandgamos/rappeurContainer.svg'
 
 export const Container = styled.div`
   display: flex;
@@ -8,7 +10,6 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
 `
-
 export const SecondContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,26 +18,151 @@ export const SecondContainer = styled.div`
   width: 30%;
   height: 100%;
 `
-
 export const RappeurContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-  width: 40%;
-  border-radius: 20px;
-  background-color: ${colors.red};
+ 
+  background-size: contain;
+  background-image: url(${RapperContainerBG});
+  background-repeat: no-repeat;
 `
 export const InputContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
-  margin-top: 10%;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
+  height: 100%;
 `
 export const SearchContainer = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
   margin-top: 10%;
   width: 100%;
 `
+export const Text = styled.p`
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 24px;
+`
+export const RapperInput = styled.input`
+  text-align: center;
+  background-color: ${colors.yellow};
+  border: 0px solid #000000;
+  border-radius: 20px;
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 24px;
+  font-weight: 700;
+  color: white;
+  ::placeholder {
+    color: white;
+    text-align: 'center';
+    font-family: 'Montserrat', 'sans-serif';
+    font-size: 24px;
+    font-weight: 700;
+    width: 100%;
+    text-transform: none;
+  }
+  &:hover {
+    text-underline-position: below;
+    text-decoration: underline;
+  }
+  ::-webkit-search-cancel-button {
+    position: relative;
+    right: 15px;
+    top: 50%;
+  }
+  width: 200px;
+  height: 60px;
+  background-size: contain;
+  background-image: url(${RapperInputBG});
+  background-repeat: no-repeat;
+`
+export const SearchInput = styled.input`
+  text-align: center;
+  background-color: ${colors.yellow};
+  border: 0px solid #000000;
+  border-radius: 20px;
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 24px;
+  font-weight: 700;
+  color: white;
+  ::placeholder {
+    color: white;
+    text-align: 'center';
+    font-family: 'Montserrat', 'sans-serif';
+    font-size: 24px;
+    font-weight: 700;
+    width: 100%;
+    text-transform: none;
+  }
+  &:hover {
+    text-underline-position: below;
+    text-decoration: underline;
+  }
+  ::-webkit-search-cancel-button {
+    position: relative;
+    right: 15px;
+    top: 50%;
+  }
+  width: 200px;
+  height: 60px;
+  background-size: contain;
+  background-image: url(${RapperInputBG});
+  background-repeat: no-repeat;
+`
+
+export const NextRoundButton = styled.button`
+  background-color: ${colors.red};
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 24px;
+  font-weight: 700;
+  width: 45%;
+  border-radius: 20px;
+  color: white;
+  &:hover {
+    cursor: pointer;
+  }
+`
+export const Header = styled.div`
+  padding-top: 1%;
+  padding-bottom: 1%;
+  width: 30%;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 24px;
+  color: white;
+  border-bottom: 5px solid ${colors.yellow};
+`
+export const RappersContainer = styled.div`
+  min-height: 50px;
+  max-height: 350px;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  height: 100%;
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 24px;
+  color: ${colors.yellow};
+`
+export const TableBottom = styled.div`
+  height: 20px;
+  width: 30%;
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
+`
+
