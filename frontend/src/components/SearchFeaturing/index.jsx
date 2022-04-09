f850be429e7de09499a9a9843b773cbca98f93e5
francoisdomecq <fradomecq@gmail.com>
2022-04-09 11:03:49 +0200

    SearchFeaturing component

---
+++ frontend/src/components/SearchFeaturing/index.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+
+import {
+  SearchContainer,
+  InputContainer,
+  SearchInput,
+  SearchLogos,
+} from './styles'
+import { TextBlue } from '../../utils/styles/Text'
+import MusicBrainzLogo from '../../assets/rollandgamos/MusicBrainz_Logo.png'
+import YoutubeLogo from '../../assets/rollandgamos/icons8-youtube.svg'
+import RapGeniusLogo from '../../assets/rollandgamos/RapGenius_Logo.png'
+
+function SearchFeaturing() {
+  const [rappeur1Search, setRappeur1Search] = useState('')
+  const [rappeur2Search, setRappeur2Search] = useState('')
+
+  return (
+    <SearchContainer>
+      <TextBlue>Un doute sur un featuring ? </TextBlue>
+      <InputContainer>
+        <SearchInput
+          type="search"
+          placeholder={rappeur1Search}
+          value={rappeur1Search}
+          onChange={(e) => setRappeur1Search(e.target.value)}
+        />
+        <SearchInput
+          type="search"
+          placeholder="rappeur 2"
+          onChange={(e) => setRappeur2Search(e.target.value)}
+        />
+      </InputContainer>
+      <SearchLogos>
+        <a
+          rel="noreferrer"
+          href={`https://www.youtube.com/results?search_query=${rappeur1Search}+${rappeur2Search}`}
+          target="_blank"
+        >
+          <img
+            style={{ width: 96, height: 96 }}
+            src={YoutubeLogo}
+            alt="Chercher sur youtube"
+          />
+        </a>
+        <a
+          rel="noreferrer"
+          href={`https://musicbrainz.org/search?query=${rappeur1Search}%20feat.%20${rappeur2Search}&type=recording&limit=5&method=advanced`}
+          target="_blank"
+        >
+          <img
+            src={MusicBrainzLogo}
+            style={{ width: 96, heigth: 96 }}
+            alt="Chercher sur MusicBrainz"
+          />
+        </a>
+        <a
+          rel="noreferrer"
+          href={`https://genius.com/search?q=${rappeur1Search}%20${rappeur2Search}`}
+          target="_blank"
+        >
+          <img
+            src={RapGeniusLogo}
+            style={{ width: 96, heigth: 96 }}
+            alt="Chercher sur RapGenius"
+          />
+        </a>
+      </SearchLogos>
+    </SearchContainer>
+  )
+}
+export default SearchFeaturing
---
+++ frontend/src/components/SearchFeaturing/styles.jsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import colors from '../../utils/styles/colors'
+
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 45%;
+  margin-bottom: 2%;
+  margin-top: 2%;
+  border-radius: 20px;
+  background: rgb(255, 255, 24);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 24, 1) 0%,
+    rgba(255, 210, 34, 1) 100%
+  );
+`
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`
+
+export const SearchInput = styled.input`
+  text-align: center;
+  width: 40%;
+  border: 0px solid #000000;
+  border-radius: 10px;
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 28px;
+  font-weight: 700;
+  color: white;
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
+  background-color: ${colors.yellow};
+  ::placeholder {
+    color: white;
+    text-align: 'center';
+    font-family: 'Montserrat', 'sans-serif';
+    font-size: 28px;
+    font-weight: 700;
+    width: 100%;
+    text-transform: none;
+  }
+  &:hover {
+    text-underline-position: below;
+    text-decoration: underline;
+  }
+  ::-webkit-search-cancel-button {
+    position: relative;
+    right: 15px;
+    top: 50%;
+  }
+`
+
+export const SearchLogos = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 60%;
+  margin-top: 3%;
+`
+export const Text = styled.p`
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 24px;
+`
