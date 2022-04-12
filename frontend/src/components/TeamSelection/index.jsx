4b4c5e8fe2acce4e6753219e38d01e39966b83a2
francoisdomecq <fradomecq@gmail.com>
2022-04-12 19:43:22 +0200

    TeamSelection component

---
+++ frontend/src/components/TeamSelection/index.jsx
@@ -0,0 +1,26 @@
+import { ContainerTeamSelection, ContainerTeam, ContainerRow } from './styles'
+import { TextBlue, TextWhite } from '../../utils/styles/Text'
+
+function TeamSelection({team1, team2, teamAnswering, setTeamAnswering}) {
+  return (
+    <ContainerTeamSelection>
+      <TextBlue>Choisir l'équipe qui répond</TextBlue>
+      <ContainerRow>
+        <ContainerTeam
+          isSelected={teamAnswering === team1}
+          onClick={() => setTeamAnswering(team1)}
+        >
+          <TextWhite size={18}>{team1}</TextWhite>
+        </ContainerTeam>
+        <ContainerTeam
+          isSelected={teamAnswering === team2}
+          onClick={() => setTeamAnswering(team2)}
+        >
+          <TextWhite size={18}>{team2}</TextWhite>
+        </ContainerTeam>
+      </ContainerRow>
+    </ContainerTeamSelection>
+  )
+}
+
+export default TeamSelection
---
+++ frontend/src/components/TeamSelection/styles.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import colors from '../../utils/styles/colors'
+
+export const ContainerRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 100%;
+  width: 100%;
+`
+export const ContainerColumn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`
+export const ContainerTeamSelection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2%;
+  padding: 2% 2% 4% 2%;
+  width: 100%;
+  border-radius: 20px;
+  background: rgb(255, 255, 24);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 24, 1) 0%,
+    rgba(255, 210, 34, 1) 100%
+  );
+  opacity: 1;
+  box-shadow: 1px 1px 1px ${colors.blue};
+`
+
+export const ContainerTeam = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  width: 45%;
+  opacity: ${({ isSelected }) => (isSelected ? 0.5 : 1)};
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
+  &:hover {
+    cursor: pointer;
+  }
+`
