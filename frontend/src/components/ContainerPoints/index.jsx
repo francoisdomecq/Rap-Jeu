530582afd2bd502e5725d1ec9996e6f16faf37f3
francoisdomecq <fradomecq@gmail.com>
2022-04-19 18:43:54 +0200

    ContainerPoints component

--- frontend/src/components/ContainerPoints/index.jsx
+++ frontend/src/components/ContainerPoints/index.jsx
@@ -7,13 +7,20 @@ import {
 import { TextBlue } from '../../utils/styles/Text'
 
 //Ce composant est utilisé dans les épreuves Rolland Gamos et les enchères pour décider du nombre de points que rapport l'épreuve
-function ContainerPoints({ updatePoints }) {
+function ContainerPoints({ updatePoints, game }) {
   return (
     <ContainerScoreSelection>
       <ContainerColumn>
         <TextBlue>Combien de points ?</TextBlue>
         <ContainerScore style={{ marginTop: '1%' }}>
-          <ButtonScore type="number" onKeyPress={(e) => updatePoints(e)} />
+          <ButtonScore
+            type="number"
+            onKeyPress={(e) =>
+              game === 'Les enchères'
+                ? updatePoints(e.target.value)
+                : updatePoints(e)
+            }
+          />
         </ContainerScore>
       </ContainerColumn>
     </ContainerScoreSelection>
