9bce69351c79ea9e733300b35771552cb5e5d00e
francoisdomecq <fradomecq@gmail.com>
2022-04-19 17:01:45 +0200

    Timer component

--- frontend/src/components/Timer/index.jsx
+++ frontend/src/components/Timer/index.jsx
@@ -1,9 +1,11 @@
 import { ContainerTimer, Time } from './styles'
 
+//Ce composant est utilisé dans les épreuves TOP 5 et les enchères pour afficher le chronomètre
 function Timer({ counter }) {
   return (
     <ContainerTimer>
       <Time>
+        {/*Si le chrono passe en dessous de 10s, on rajoute un 0 pour que le texte reste centré*/}
         00:{counter < 10 ? '0' : null}
         {counter}
       </Time>
