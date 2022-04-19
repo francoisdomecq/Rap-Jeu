5aa2d87b86dc7f779f8570f7d1f466d2121acc89
francoisdomecq <fradomecq@gmail.com>
2022-04-19 23:00:14 +0200

    Header responsive

--- frontend/src/components/Header/styles.jsx
+++ frontend/src/components/Header/styles.jsx
@@ -7,11 +7,20 @@ export const NavContainer = styled.nav`
   justify-content: space-between;
   background-color: #2704c4;
   color: white;
+  @media screen and (min-width: 300px) and (max-width: 600px) {
+    padding: 10px;
+  }
 `
 export const PageText = styled.span`
   font-family: 'Montserrat', sans-serif;
   font-weight: 700;
   font-size: 18px;
+  @media screen and (min-width: 300px) and (max-width: 600px) {
+    font-size: 6px;
+  }
+  @media screen and (min-width: 600px) and (max-width: 900px) {
+    font-size: 12px;
+  }
 `
 
 export const PageTextBis = styled.span`
@@ -26,11 +35,21 @@ export const PageTextBis = styled.span`
   -moz-text-fill-color: transparent;
   background-clip: text;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  @media screen and (min-width: 300px) and (max-width: 600px) {
+    font-size: 10px;
+  }
+  @media screen and (min-width: 600px) and (max-width: 900px) {
+    font-size: 16px;
+  }
 `
 export const Logo = styled.img`
   width: 24px;
-  height:24px;
-  &:hover{
-    cursor:pointer
+  height: 24px;
+  &:hover {
+    cursor: pointer;
+  }
+  @media screen and (min-width: 300px) and (max-width: 900px) {
+    width: 18px;
+    height: 18px;
   }
 `
