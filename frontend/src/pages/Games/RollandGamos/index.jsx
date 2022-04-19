6d0c0dd7699aaa238c49e3050db160e3f574f4dd
francoisdomecq <fradomecq@gmail.com>
2022-04-19 19:00:04 +0200

    Rolland Gamos page

--- frontend/src/pages/Games/RollandGamos/index.jsx
+++ frontend/src/pages/Games/RollandGamos/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState,useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Confetti from 'react-confetti'
 import { GameContext, TeamContext } from '../../../utils/context'
@@ -6,132 +6,141 @@ import { GameContext, TeamContext } from '../../../utils/context'
 import RappeurArray from '../../../components/Rappers'
 import SearchFeaturing from '../../../components/SearchFeaturing'
 import TeamSelection from '../../../components/TeamSelection'
+import ContainerPoints from '../../../components/ContainerPoints'
 
 import {
-  RappeurContainer,
+  ContainerRapper,
   SecondContainer,
+  ContainerRow,
+  ContainerColumn,
   Text,
   RapperInput,
-  ContainerTeamSelection,
-  ContainerScore,
-  ButtonScore,
   ContinuerContainer,
 } from './styles'
+
 import { TextBlue } from '../../../utils/styles/Text'
-import { ContainerRow, ContainerColumn } from '../../../utils/styles/Containers'
-
-
 
 function RollandGamos() {
+  //Rappeur sélectionné
   const [rappeur, setRappeur] = useState('')
+  //Compteur de manche
   const [count, setCount] = useState(0)
+  //Points que remporte une manche
   const [points, setPoints] = useState()
   const [teamWinner, setTeamWinner] = useState('')
-  const { resetGame,setTeamAnswering } = useContext(GameContext)
+  const { resetGame, setTeamAnswering } = useContext(GameContext)
   const { team1, team2, updateScore, scoreTeam1, scoreTeam2, resetTeams } =
     useContext(TeamContext)
 
   function selectRappeur(rappeur) {
     setRappeur(rappeur)
   }
-  const updatePoints = (e) => {
+  function updatePoints(e) {
     if (e.key === 'Enter') {
       setPoints(e.target.value)
     }
   }
-  useEffect(() => {
-    setTeamAnswering()
-  }, [])
 
+  //Cette fonction permet de s'assurer que la variable rappeur ne soit jamais 'vide' une fois une manche commencée
+  //Sinon, lorsque le maître de jeu supprimerait un rappeur de l'input, il serait redirigé vers l'écran de choix d'un rappeur
   function lastRapper(e) {
     if (
+      //Si le type d'évènement est 'supprimer' et que la longueur de rappeur est de 1, alors on remplace rappeur par un espace
       e.nativeEvent.inputType === 'deleteContentBackward' &&
       rappeur.length === 1
     )
       setRappeur(' ')
     else setRappeur(e.target.value)
   }
-  function newRappeur() {
+
+  //Cette fonction permet de passer à une nouvelle manche
+  function nextRound() {
+    //On ajoute les points au gagnant
     if (teamWinner === team1) updateScore(points, team1)
     else if (teamWinner === team2) updateScore(points, team2)
+    //On réinitialise la variable rappeur
     setRappeur('')
+    //On augmente le nombre de manches
     setCount(count + 1)
+    //On réinitialise l'équipe gagnante
     setTeamWinner()
+    //On réinitialise le nombre de points que rapporte la manche
     setPoints()
   }
+
+  //Cette fonction est utilisée lorsque l'utilisateur souhaite recommencer une nouvelle partie.
   function reset() {
     localStorage.clear()
     resetGame()
     resetTeams()
   }
 
-  return count < 1 ? (
-    <ContainerRow style={{ marginBottom: '2%' }}>
-      <ContainerColumn>
-        <ContainerRow>
-          {rappeur && points ? (
-            <ContainerColumn>
-              <RappeurContainer style={{ marginTop: '2%' }}>
-                <Text style={{ color: 'white' }}>Dernier rappeur cité</Text>
-                <RapperInput
-                  type="search"
-                  placeholder={rappeur}
-                  onChange={(e) => lastRapper(e)}
-                />
-              </RappeurContainer>
-              <ContainerRow style={{ width: '40%' }}>
-                <SearchFeaturing rappeur={rappeur} />
-              </ContainerRow>
+  //La variable teamAnswering étant globale à l'application, lors du premier chargement de la page, on la réinitialise pour qu'une
+  //équipe ne soit pas sélectionnée en fonction des épreuves passées
+  useEffect(() => {
+    setTeamAnswering()
+  }, [setTeamAnswering])
 
-              <ContainerColumn style={{ width: '40%' }}>
-                <TeamSelection
-                  team1={team1}
-                  team2={team2}
-                  teamAnswering={teamWinner}
-                  game="Rolland Gamos"
-                  setTeamAnswering={setTeamWinner}
-                />
-              </ContainerColumn>
-              <ContainerRow style={{ marginTop: '2%' }}>
-                <ContinuerContainer
-                  isTeamSelected={teamWinner ? true : false}
-                  onClick={() => newRappeur()}
-                >
-                  {count < 2 ? 'Manche suivante' : 'Résultats'}
-                </ContinuerContainer>
-              </ContainerRow>
+  return count < 3 ? (
+    //Si moins de 3 manches ont été jouées, on affiche tout ce qui permet au maître de jeu d'animer la partie. Sinon, on affiche l'écran de victoire
+    <ContainerRow style={{ marginBottom: '2%' }}>
+      {
+        //Si un rappeur a été sélectionné et un nombre de points définis, alors on affiche l'écran de jeu
+        rappeur && points ? (
+          <ContainerColumn>
+            <ContainerRapper style={{ marginTop: '2%' }}>
+              <Text style={{ color: 'white' }}>Dernier rappeur cité</Text>
+              <RapperInput
+                type="search"
+                placeholder={rappeur}
+                onChange={(e) => lastRapper(e)}
+              />
+            </ContainerRapper>
+            <ContainerRow style={{ width: '40%' }}>
+              <SearchFeaturing rapper={rappeur} />
+            </ContainerRow>
+
+            <ContainerColumn style={{ width: '40%' }}>
+              <TeamSelection
+                team1={team1}
+                team2={team2}
+                teamAnswering={teamWinner}
+                game="Rolland Gamos"
+                setTeamAnswering={setTeamWinner}
+              />
             </ContainerColumn>
-          ) : (
-            <ContainerColumn>
+            <ContainerRow style={{ marginTop: '2%' }}>
+              <ContinuerContainer
+                isTeamSelected={teamWinner ? true : false}
+                onClick={() => (teamWinner ? nextRound() : null)}
+              >
+                {count < 2 ? 'Manche suivante' : 'Résultats'}
+              </ContinuerContainer>
+            </ContainerRow>
+          </ContainerColumn>
+        ) : (
+          //Sinon, on affiche l'écran de sélection d'un rappeur et du nombre de points
+          <ContainerRow>
+            <SecondContainer>
               <Text>Manche n°{count + 1}</Text>
-              <ContainerRow>
-                <SecondContainer>
-                  <RappeurArray
-                    page="rappeur"
-                    selectRapper={selectRappeur}
-                    chosenRapper={rappeur}
-                  />
-                </SecondContainer>
-                <ContainerColumn style={{ width: '35%' }}>
-                  <ContainerTeamSelection>
-                    <ContainerColumn>
-                      <TextBlue>Combien de points ?</TextBlue>
-                      <ContainerScore style={{ marginTop: '1%' }}>
-                        <ButtonScore
-                          type="number"
-                          onKeyPress={(e) => updatePoints(e)}
-                        />
-                      </ContainerScore>
-                    </ContainerColumn>
-                  </ContainerTeamSelection>
-                </ContainerColumn>
-              </ContainerRow>
+              <RappeurArray
+                page="rappeur"
+                selectRapper={selectRappeur}
+                chosenRapper={rappeur}
+              />
+            </SecondContainer>
+            <ContainerColumn style={{ width: '35%' }}>
+              <ContainerPoints
+                game="Rolland Gamos"
+                updatePoints={updatePoints}
+              />
             </ContainerColumn>
-          )}
-        </ContainerRow>
-      </ContainerColumn>
+          </ContainerRow>
+        )
+      }
     </ContainerRow>
   ) : (
+    //Ecran de fin de partie
     <ContainerColumn>
       <Confetti
         width={window.innerWidth}
@@ -139,7 +148,11 @@ function RollandGamos() {
         colors={['#2704C4', '#ff003d', '#ffd222']}
       />
       <TextBlue style={{ marginTop: '3.5%' }}>
-        Bien joué {scoreTeam1 < scoreTeam2 ? team2 : team1}{' '}
+        {scoreTeam1 < scoreTeam2
+          ? `Bien joué ${team2}`
+          : scoreTeam1 === scoreTeam2
+          ? 'Egalité, vous allez devoir rejouer !'
+          : `Bien joué ${team1}`}
       </TextBlue>
 
       <ContinuerContainer
--- frontend/src/pages/Games/RollandGamos/styles.jsx
+++ frontend/src/pages/Games/RollandGamos/styles.jsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
 import colors from '../../../utils/styles/colors'
-import RapperInputBG from '../../../assets/rollandgamos/rappeurInput.svg'
-import RapperContainerBG from '../../../assets/rollandgamos/rappeurContainer.svg'
-import rectangle from '../../../assets/rectangle.svg'
 import Continuer from '../../../assets/continuer.svg'
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
@@ -11,6 +9,24 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
 `
+export const ContainerRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 100%;
+  width: 100%;
+`
+export const ContainerColumn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`
+
 export const SecondContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -19,16 +35,15 @@ export const SecondContainer = styled.div`
   width: 50%;
   height: 100%;
 `
-export const RappeurContainer = styled.div`
+export const ContainerRapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-  width: 300px;
-  height: 150px;
-  background-size: contain;
-  background-image: url(${RapperContainerBG});
-  background-repeat: no-repeat;
+  border-radius: 20px;
+  border: 2px solid ${colors.yellow};
+  padding: 1%;
+  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
   opacity: 0.95;
 `
 export const InputContainer = styled.div`
@@ -79,105 +94,7 @@ export const RapperInput = styled.input`
   }
   width: 100%;
 `
-export const SearchInput = styled.input`
-  text-align: center;
-  background-color: ${colors.yellow};
-  border: 0px solid #000000;
-  border-radius: 20px;
-  font-family: 'Montserrat', 'sans-serif';
-  font-size: 24px;
-  font-weight: 700;
-  color: white;
-  ::placeholder {
-    color: white;
-    text-align: 'center';
-    font-family: 'Montserrat', 'sans-serif';
-    font-size: 24px;
-    font-weight: 700;
-    width: 100%;
-    text-transform: none;
-  }
-  &:hover {
-    text-underline-position: below;
-    text-decoration: underline;
-  }
-  ::-webkit-search-cancel-button {
-    position: relative;
-    right: 15px;
-    top: 50%;
-  }
-  width: 200px;
-  height: 60px;
-  background-size: contain;
-  background-image: url(${RapperInputBG});
-  background-repeat: no-repeat;
-`
 
-export const NextRoundButton = styled.button`
-  background-color: ${colors.red};
-  font-family: 'Montserrat', 'sans-serif';
-  font-size: 24px;
-  font-weight: 700;
-  width: 45%;
-  border-radius: 20px;
-  color: white;
-  &:hover {
-    cursor: pointer;
-  }
-`
-export const ContainerTeam = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 10px;
-  width: 45%;
-  background: linear-gradient(90deg, #15008d 0%, #198bf5 100%);
-  opacity: ${({ isSelected }) => (isSelected ? '0.4' : '1')};
-  &:hover {
-    cursor: pointer;
-  }
-`
-export const ContainerTeamSelection = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2%;
-  padding: 2%;
-  width: 100%;
-  border-radius: 20px;
-  background: rgb(255, 255, 24);
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 24, 1) 0%,
-    rgba(255, 210, 34, 1) 100%
-  );
-  opacity: 1;
-  box-shadow: 1px 1px 1px ${colors.blue};
-`
-
-export const ContainerScore = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  width: 189px;
-  height: 90px;
-  background-size: contain;
-  background-image: url(${rectangle});
-  background-repeat: no-repeat;
-`
-export const ButtonScore = styled.input`
-  width: 100%;
-  background-color: transparent;
-  border: none;
-  outline: none;
-  color: white;
-  text-align: center;
-  font-size: 30px;
-  font-weight: bold;
-  ::placeholder {
-    color: white;
-  }
-`
 export const ContinuerContainer = styled.div`
   display: flex;
   align-items: center;
@@ -197,21 +114,4 @@ export const ContinuerContainer = styled.div`
   }
   padding-right: 1%;
   opacity: ${({ isTeamSelected }) => (isTeamSelected ? 1 : 0.5)};
-`
-
-export const CongratsContainer = styled.div`
-  background-image: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 1) 36%,
-    rgb(255, 255, 255) 100%
-  );
-  background-size: 100%;
-  background-repeat: repeat;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -moz-background-clip: text;
-  background-clip: text;
-  -moz-text-fill-color: transparent;
-  -webkit-text-stroke-width: 0px;
-  -webkit-text-stroke-color: rgb(255, 255, 255);
 `
