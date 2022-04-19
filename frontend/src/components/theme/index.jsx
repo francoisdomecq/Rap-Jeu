74d7af4f6e1a3321d1bd3353721c23081b022152
francoisdomecq <fradomecq@gmail.com>
2022-04-19 17:00:14 +0200

    Theme component

--- frontend/src/components/theme/index.jsx
+++ frontend/src/components/theme/index.jsx
@@ -10,19 +10,23 @@ import {
   TableContainer,
   TableBottom,
 } from './styles'
-import { LoaderWrapper, Loader } from '../../utils/styles/Atoms'
-import Redbull from '../../assets/PNG/redbull.png'
 import { TextBlue } from '../../utils/styles/Text'
 
+import { LoaderWrapper, Loader } from '../../utils/styles/Atoms'
+
+import Redbull from '../../assets/PNG/redbull.png'
+
+//Ce composant permet d'afficher le tableau des thèmes pour les épreuves TOP 5 et Les enchères
 function Theme({ selectTheme, page, chosenTheme }) {
   const [isDataLoading, setDataLoading] = useState(true)
   const [theme, setTheme] = useState([])
 
+  //Cette fonction permet d'afficher les thèmes dans le tableau
   function displayThemes() {
     return theme.map((theme) => (
       <LineContainer key={theme._id}>
         <Line isChosen={theme === chosenTheme ? true : false}>
-          <QuestionTheme  onClick={() => selectTheme(theme)}>
+          <QuestionTheme onClick={() => selectTheme(theme)}>
             {theme.theme}
           </QuestionTheme>
         </Line>
@@ -31,23 +35,27 @@ function Theme({ selectTheme, page, chosenTheme }) {
   }
 
   useEffect(() => {
+    //On effectue le call API pour récupérer les données. L'adresse est modifiée en fonction de la page sur laquelle l'utilisateur se trouve
     fetch(`http://localhost:3001/api/${page}`)
       .then((response) => response.json())
       .then((requestData) => {
+        //On ajoute les données récupérées dans le tableau theme
         setTheme(requestData)
+        //On dit que les données ont chargé pour arrêter d'afficher le loader
         setDataLoading(false)
       })
       .catch((error) => console.log(error))
-  }, [])
+  }, [page])
 
   return isDataLoading ? (
+    //Si les données chargent, on affiche le loader
     <LoaderWrapper>
       <Loader src={Redbull} />
     </LoaderWrapper>
   ) : (
     <Container>
       <TableHead>
-          <TextBlue>Choisir un theme</TextBlue>
+        <TextBlue>Choisir un theme</TextBlue>
       </TableHead>
       <TableContainer>
         <Table>
